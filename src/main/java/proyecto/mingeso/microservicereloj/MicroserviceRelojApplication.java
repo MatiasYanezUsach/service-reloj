65015021f7c3609a77234d910843520b7aa7c8fa
Matias Yanez <matias.yanez@usach.cl>
2022-11-05 14:58:56 -0300

    inicializacion del microservicio reloj

---
+++ src/main/java/proyecto/mingeso/microservicereloj/MicroserviceRelojApplication.java
@@ -0,0 +1,13 @@
+package proyecto.mingeso.microservicereloj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceRelojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceRelojApplication.class, args);
+	}
+
+}
---
+++ src/test/java/proyecto/mingeso/microservicereloj/MicroserviceRelojApplicationTests.java
@@ -0,0 +1,13 @@
+package proyecto.mingeso.microservicereloj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceRelojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
