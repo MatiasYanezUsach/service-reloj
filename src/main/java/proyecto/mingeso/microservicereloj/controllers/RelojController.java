0d70fc97f45e5b034c80497179be3bc98c6d0882
Matias Yanez <matias.yanez@usach.cl>
2022-11-20 20:01:11 -0300

    crossOrigin

--- src/main/java/proyecto/mingeso/microservicereloj/controllers/RelojController.java
+++ src/main/java/proyecto/mingeso/microservicereloj/controllers/RelojController.java
@@ -13,6 +13,7 @@ import java.util.Objects;
 
 @RestController
 @RequestMapping("/marcas")
+@CrossOrigin(origins = "http://localhost:3000")
 public class RelojController {
     @Autowired
     RelojService relojService;
